916e5e1a2ea0d81517d37b21762341b3db0c2a6a
Gabriel Martins <gabrielcust@gmail.com>
2023-03-28 15:49:52 -0300

    JS - Variáveis

---
+++ 1.fundamentos/secao-03-introducao-a-javascript/scripts.js
@@ -0,0 +1,6 @@
+let myName = "Gabriel";
+let birthCity = "Fortaleza";
+let birthYear = `${myName} +  nasceu em 1994.`;
+console.log(`Nome: ${myName}`);
+console.log(`Cidade Natal: ${birthCity}`);
+console.log(birthYear);
