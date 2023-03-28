9c5399bd002ac2632b94107b91f3a314a0964f1c
Gabriel Martins <gabrielcust@gmail.com>
2023-03-28 15:58:58 -0300

    if/else

--- 1.fundamentos/secao-03-introducao-a-javascript/scripts.js
+++ 1.fundamentos/secao-03-introducao-a-javascript/scripts.js
@@ -1,6 +1,10 @@
-let base = 5;
-let height = 8;
-let area = base * height;
-console.log(area);
-let perimeter = base * 2 + height * 2;
-console.log(perimeter);
+let rating = 61;
+if (rating >= 80 && rating <= 100) {
+  console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");
+} else if (rating < 80 && rating >= 60) {
+  console.log("Você está na lista de espera!");
+} else if (rating < 60 && rating >= 1) {
+  console.log("Infelizmente você foi reprovado!");
+} else {
+  console.log("Informação inválida");
+}
