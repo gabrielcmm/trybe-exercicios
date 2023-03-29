cb0b5d7d777c5aa8fb19940f29fb682dc4af3ca9
Gabriel Martins <gabrielcust@gmail.com>
2023-03-29 17:58:35 -0300

    final

--- 1.fundamentos/secao-03-introducao-a-javascript/dia-02-Javascript-array-e-loop-for/scripts.js
+++ 1.fundamentos/secao-03-introducao-a-javascript/dia-02-Javascript-array-e-loop-for/scripts.js
@@ -1,5 +1,7 @@
-let names = ["João", "Maria", "Antônio", "Margarida"];
-
-for (const name of names) {
-  console.log(name);
+let dado1 = Math.ceil(Math.random() * 6);
+console.log("resultado dado 1:", dado1);
+let dado2 = Math.ceil(Math.random() * 6);
+while (dado1 !== dado2) {
+  dado2 = Math.ceil(Math.random() * 6);
+  console.log("resultado dado 2:", dado2);
 }
