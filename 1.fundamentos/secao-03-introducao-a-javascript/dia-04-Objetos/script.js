e383bc385bc22bc353e911e928c281b2cb04dc93
Gabriel Martins <gabrielcust@gmail.com>
2023-04-03 15:26:29 -0300

    1-3.4 adicionando propriedades

--- 1.fundamentos/secao-03-introducao-a-javascript/dia-04-Objetos/script.js
+++ 1.fundamentos/secao-03-introducao-a-javascript/dia-04-Objetos/script.js
@@ -1,23 +1,12 @@
-let player = {
-  name: "Marta",
-  lastName: "Silva",
-  age: 34,
-  medals: {
-    golden: 2,
-    silver: 3,
-  },
-};
+let student = {};
 
-console.log(
-  `A Jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade.`
-);
+function addProperty(whichArray, whichProperty, whichValue) {
+  whichArray[whichProperty] = whichValue;
+}
 
-player["bestInTheWorld"] = [2006, 2007, 2008, 2009, 2010, 2018];
+addProperty(student, "firstName", "Gabriel");
+addProperty(student, "lastName", "Martins");
+addProperty(student, "phone", "(85) 95555-5555");
+addProperty(student, "mail", "gabriel@trybestudent.com");
 
-console.log(
-  `A Jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes.`
-);
-
-console.log(
-  `A Jogadora possui ${player.medals.golden} medalhas de Ouro e ${player.medals.silver} medalhas de prata.`
-);
+console.table(student);
